3d606a26fa4a00c9b67a6c6fc4d5a039f80d0fbd
n.krylov <66363299+general-rj45@users.noreply.github.com>
2023-08-24 19:02:30 +0300

    Update MPLPipelineTest.groovy

--- vars/MPLPipelineTest.groovy
+++ vars/MPLPipelineTest.groovy
@@ -1,1 +1,25 @@
-println "Hello World"
+def call(body) {
+  def MPL = MPLPipelineConfig(body, [
+    agent_label: '',
+    modules: [
+      Checkout: [:],
+    ]
+  ])
+
+  pipeline {
+    agent {
+      label MPL.agentLabel
+    }
+    options {
+      skipDefaultCheckout(true)
+    }
+    stages {
+      stage( 'Checkout' ) {
+        when { expression { MPLModuleEnabled() } }
+        steps {
+          MPLModule()
+        }
+      }
+    }
+  }
+}
